7d6733d7fd72c2b289f993cbe3bfa36e186e40b5
alexfias <kies@fias.uni-frankfurt.de>
2020-04-01 11:52:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,28 +25,24 @@ def derivatives(S, E, I, params):
     
     return dS, dE, dI
 
-def solve(nodes=nodes, pop=pop, t=0, params=0,snapshots=1):
-    S=pop[node + ' S']
-    E=pop[node + ' E']
-    I=pop[node + ' I']
-    R=pop[node + ' R']
-    dt = 1
-
-    for d_t in range(len(snapshots)-1):
-        
-        dS, dE, dI = derivatives(S[d_t], E[d_t], I[d_t], params)
-        S = np.append(S,[S[d_t]+dt*dS])
-        E = np.append(E,[E[d_t]+dt*dE])
-        I = np.append(I,[I[d_t]+dt*dI])
-        R = np.append(R,[1-I[d_t]-S[d_t]-E[d_t]])
-
+def solve(nodes, pop, t=0, params=0,snapshots=1):
     
-    return S,E,I,R
+    dt = 1
+    d_t_minus = snapshots[0]
+    for d_t in snapshots[1:]:
+        for node in nodes:  
+            dS, dE, dI = derivatives(pop[node+' S'][d_t_minus], pop[node+' E'][d_t_minus], pop[node+' I'][d_t_minus], params)
+            pop[node+' S'][d_t] = pop[node+' S'][d_t_minus]+dt*dS
+            pop[node+' E'][d_t] = pop[node+' E'][d_t_minus]+dt*dE
+            pop[node+' I'][d_t] = pop[node+' I'][d_t_minus]+dt*dI
+            pop[node+' R'][d_t] = 1-pop[node+' I'][d_t]-pop[node+' S'][d_t]-pop[node+' E'][d_t]
+        migration(pop,params)
+        d_t_minus = d_t
+    return pop
     
 def initialise_pop(nodes=np.array(['0']),snapshots=np.array(['0']),total_pop={'0':100000.}):
     #initialise populations of different nodes
     
-    N=100000.
     index = ['S','E','I','R']
     pop_index={'S':1.,'E':0.,'I':1.,'R':0}
     nodes_t = np.array([])
@@ -55,38 +51,42 @@ def initialise_pop(nodes=np.array(['0']),snapshots=np.array(['0']),total_pop={'0
         nodes_t=np.append(nodes_t,[node+ ' ' +i for node in nodes])
     pop = pd.DataFrame(columns=nodes_t,index=snapshots)
     for node in nodes:
-        pop[node +' '+ 'S'].loc[snapshots[0]]=pop_index['S']
+        pop[node +' '+ 'S'].loc[snapshots[0]]=pop_index['S']-pop_index['I']/total_pop[node]
         pop[node +' '+ 'E'].loc[snapshots[0]]=pop_index['E']
         pop[node +' '+ 'I'].loc[snapshots[0]]=pop_index['I']/total_pop[node]
-        pop[node +' '+ 'E'].loc[snapshots[0]]=pop_index['R']
+        pop[node +' '+ 'R'].loc[snapshots[0]]=pop_index['R']
         
     return pop    
 
-def plot_results(S,E,I,R,snapshots):
+def plot_results(pop,snapshots):
+    for node in nodes:
+        fig, ax = plt.subplots()
+        ax.plot(snapshots,pop[node +' '+ 'E'],label='Exposed')
+        ax.plot(snapshots,pop[node +' '+ 'S'],label='Susceptible')
+        ax.plot(snapshots,pop[node +' '+ 'I'],label='Infected')
+        ax.plot(snapshots,pop[node +' '+ 'R'],label='Recovered')
+
+        ax.set_xlim([snapshots[0],snapshots[len(snapshots)-1]])
+        ax.set_title(node)
+        plt.xticks(rotation=90)
+        plt.legend()
+
+        plt.show()
     
-    fig, ax = plt.subplots()
-    ax.plot(snapshots,E,label='Exposed')
-    ax.plot(snapshots,S,label='Susceptible')
-    ax.plot(snapshots,I,label='Infected')
-    ax.plot(snapshots,R,label='Recovered')
-
-    ax.set_xlim([snapshots[0],snapshots[len(snapshots)-1]])
-    plt.xticks(rotation=90)
-    plt.legend()
-
-    plt.show()
-
-
+def migration(pop,params):
+    #internal and external migration
+    
+    return pop
 
 #main
 params = load_params()
 
 snapshots = pd.date_range('2020-01-01', periods=500, freq='d')
 
-nodes=['0']
-pop=initialise_pop(nodes=nodes)
+nodes=['Berlin','Bayern','Baden Württemberg']
+pop=initialise_pop(nodes=nodes,snapshots=snapshots,total_pop={'Berlin':100000.,'Bayern':100000.,'Baden Württemberg':100000.})
 
-for node in nodes:
-    S,E,I,R=solve(pop=pop,params=params,snapshots=snapshots)
 
-plot_results(S,E,I,R,snapshots)
+pop=solve(nodes=nodes,pop=pop,params=params,snapshots=snapshots)
+
+plot_results(pop,snapshots)
